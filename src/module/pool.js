d14a98f1d44664687fb81381e843bdda5e0b812c
Samarth Chauhan <samarth.chauhan13022002@gmail.com>
2023-08-28 23:19:49 +0530

    Update pool.js

--- src/module/pool.js
+++ src/module/pool.js
@@ -2,11 +2,10 @@ require("dotenv").config();
 const { Pool } = require("pg");
 
 const pool = new Pool({
-  user: process.env.DB_USERNAME,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: 5432,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 module.exports = pool;
